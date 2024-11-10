9fd3348afae858b6b89e6e0b69c0a2ece67936e5
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2024-11-11 00:32:38 +0900

    refactor(Answer): 상수 처리

--- src/main/java/store/domain/vo/Answer.java
+++ src/main/java/store/domain/vo/Answer.java
@@ -4,6 +4,9 @@ import static store.util.constant.ErrorMessage.INVALID_FORMAT;
 
 public class Answer {
 
+    private static final String ANSWER_YES = "Y";
+    private static final String ANSWER_NO = "N";
+
     private final String answer;
 
     private Answer(String answer) {
@@ -16,11 +19,11 @@ public class Answer {
     }
 
     public boolean isYes() {
-        return answer.equals("Y");
+        return answer.equals(ANSWER_YES);
     }
 
     private static void validate(String input) {
-        if (!(input.equals("Y") || input.equals("N"))) {
+        if (!(input.equals(ANSWER_YES) || input.equals(ANSWER_NO))) {
             throw new IllegalArgumentException(INVALID_FORMAT.getMessage());
         }
     }
